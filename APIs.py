b77f8e6ffaa5fc4c84664ad5861aea5dcfca94e1
Ângelo <angelo.parrella@live.com>
2024-07-09 10:39:40 -0300

    Att

--- APIs.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = '<seu_telegram_bot_token>'
-REPLICATE_OPENAI_RUN = '<seu_replicate_openai_run_id>'
-REPLICATE_API_TOKEN = '<seu_replicate_api_token>'
--- scr/main.py
+++ scr/main.py
@@ -1,5 +1,8 @@
 import replicate
 import telebot
+import requests
+import errors
+import os
 from APIs import BOT_TOKEN, REPLICATE_OPENAI_RUN, REPLICATE_API_TOKEN
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -19,7 +22,7 @@ def handle_audio(message):
             # Caso o áudio seja enviado como documento (alguns formatos podem vir assim)
             file_id = message.document.file_id
         else:
-            bot.reply_to(message, "Este tipo de arquivo de áudio não é suportado. Tente converte-lo!")
+            errors.handle_unsupported_file_type_error(bot, message)
             return
 
         file_info = bot.get_file(file_id)
@@ -29,7 +32,7 @@ def handle_audio(message):
         file_size = file_info.file_size
         
         # Definir a mensagem de feedback com base no tamanho do arquivo
-        if file_size > 10 * 1024 * 1024:  # Exemplo de 10 MB (em bytes)
+        if file_size > 2 * 1024 * 1024:  # Exemplo de 2 MB (em bytes)
             feedback_message = "⌛ Aguarde, estou transcrevendo seu áudio. Como o arquivo é grande, pode demorar um pouco."
         else:
             feedback_message = "⌛ Aguarde, estou transcrevendo seu áudio..."
@@ -37,18 +40,38 @@ def handle_audio(message):
         # Enviar mensagem de feedback
         sent_message = bot.send_message(message.chat.id, feedback_message)
 
-        input_data = {
-            "audio": file_url,
-            "language": "pt"
-        }
+        # Obter o diretório do script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        temp_dir = os.path.join(script_dir, 'temp')
 
-        client = replicate.Client(api_token=REPLICATE_API_TOKEN)
+        # Verificar e criar o diretório temporário se não existir
+        os.makedirs(temp_dir, exist_ok=True)
 
-        output = client.run(
-            REPLICATE_OPENAI_RUN,
-            input=input_data
-        )
-        
+        # Baixar o arquivo do Telegram
+        file_path = os.path.join(temp_dir, file_info.file_path.split('/')[-1])
+        file_data = requests.get(file_url)
+
+        # Salvar o arquivo no sistema local
+        with open(file_path, 'wb') as f:
+            f.write(file_data.content)
+
+        # Exibir o diretório do arquivo salvo
+        print(f"Arquivo salvo em: {file_path}")
+
+        # Enviar o arquivo baixado para a API do Replicate
+        with open(file_path, 'rb') as audio_file:
+            input_data = {
+                "audio": audio_file,
+                "language": "pt"
+            }
+
+            client = replicate.Client(api_token=REPLICATE_API_TOKEN)
+
+            output = client.run(
+                REPLICATE_OPENAI_RUN,
+                input=input_data
+            )
+
         transcription = ''.join(segment['text'] for segment in output['segments'])
         
         # Enviar a transcrição obtida
@@ -56,9 +79,12 @@ def handle_audio(message):
         
         # Remover a mensagem temporária
         bot.delete_message(message.chat.id, sent_message.message_id)
+        
+        # Remover o arquivo temporário local
+        os.remove(file_path)
 
     except Exception as e:
         print(f"Error: {e}")
-        bot.send_message(message.chat.id, "Ocorreu um erro ao processar o áudio. Por favor, tente novamente.")
+        errors.handle_unknown_error(bot, message)
 
 bot.polling()
